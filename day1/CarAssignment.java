a2e2cf2118a4b39b414c086af4ac8eba5dd19a2b
francis-carol <114824985+francis-carol@users.noreply.github.com>
2022-10-01 22:32:46 +0530

    Add files via upload

---
+++ day1/CarAssignment.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class CarAssignment {
+	int car=5;
+	byte kms=100;
+	short broughtyear=2022;
+	int carprice=2000000;
+	long carenginee=72292903729L;
+	float carkms=234.789F;
+	double registrationno=2893820.299029200;
+	char carmodel='C';
+	boolean automatic=false;
+	
+	
+}
+
